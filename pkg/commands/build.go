6b9702d54f2dbcfcd65f77be99f5ad69d1b94fe4
Steve McQuaid <steve@stevemcquaid.com>
2022-02-07 19:56:17 -0500

    add filePath for building non root main.go file

--- pkg/commands/build.go
+++ pkg/commands/build.go
@@ -2,16 +2,22 @@ package commands
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/stevemcquaid/mcq/pkg/shell"
 )
 
 // builds the binary
-func Build(binaryName string) error {
+func Build(binaryName string, args []string) error {
+	filePath := strings.Join(args, " ")
+
+	if len(filePath) == 0 {
+		filePath = "./"
+	}
 	return shell.OrderedRunner(
 		[]shell.RunningFunction{
 			&shell.StringFunction{
-				Arg:      fmt.Sprintf("go build -o bin/%s ./", binaryName),
+				Arg:      fmt.Sprintf("go build -o bin/%s %s", binaryName, filePath),
 				Function: shell.PrettyRun,
 			},
 			&shell.StringFunction{
@@ -22,11 +28,17 @@ func Build(binaryName string) error {
 	)
 }
 
-func BuildLinux(binaryName string) error {
+func BuildLinux(binaryName string, args []string) error {
+	filePath := strings.Join(args, " ")
+
+	if len(filePath) == 0 {
+		filePath = "./"
+	}
+
 	return shell.OrderedRunner(
 		[]shell.RunningFunction{
 			&shell.StringFunction{
-				Arg:      fmt.Sprintf("GOOS=linux GOARCH=amd64 go build -o bin/%s ./", binaryName),
+				Arg:      fmt.Sprintf("GOOS=linux GOARCH=amd64 go build -o bin/%s %s", binaryName, filePath),
 				Function: shell.PrettyRun,
 			},
 			&shell.StringFunction{
@@ -37,11 +49,17 @@ func BuildLinux(binaryName string) error {
 	)
 }
 
-func BuildWindows(binaryName string) error {
+func BuildWindows(binaryName string, args []string) error {
+	filePath := strings.Join(args, " ")
+
+	if len(filePath) == 0 {
+		filePath = "./"
+	}
+
 	return shell.OrderedRunner(
 		[]shell.RunningFunction{
 			&shell.StringFunction{
-				Arg:      fmt.Sprintf("GOOS=windows GOARCH=amd64 go build -o bin/%s ./", binaryName),
+				Arg:      fmt.Sprintf("GOOS=windows GOARCH=amd64 go build -o bin/%s %s", binaryName, filePath),
 				Function: shell.PrettyRun,
 			},
 			&shell.StringFunction{
