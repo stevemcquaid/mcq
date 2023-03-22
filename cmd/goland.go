503ffa6483cff3c6f07f4490fdc17177269fb23e
Steve McQuaid <steve@stevemcquaid.com>
2023-03-22 12:46:47 -0400

    add outline for goland fix

---
+++ cmd/goland.go
@@ -0,0 +1,20 @@
+package cmd
+
+// Goland Fix
+
+// echo "const TheVersion = `go1.17.2`" > $GOROOT/src/runtime/internal/sys/zversion.go
+
+// Need to add the following line into the file.
+//
+// Check that GOROOT exists
+//
+// parse the go version
+//
+// append and save the file
+//
+// then print out the path neded to add to goland. IE: /Users/steve/.gvm/gos/go1.19.5
+//
+// Then restart goland
+
+// ---
+// alternatively just edit the .project file directly
